b7f1d1c1371df46f4befe2bc2bb134b546bcd337
Emily Brown <emi.jbrown@gmail.com>
2023-05-24 12:19:19 -0400

    added review model and validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,6 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
   CATEGORY = %w[chinese italian japanese french belgian]
 
   validates :name, :address, :category, presence: true
