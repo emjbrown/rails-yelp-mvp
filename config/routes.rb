92be172cc234f455619613947b3820b9314391de
Emily Brown <emi.jbrown@gmail.com>
2023-05-24 16:17:49 -0400

    added review new action/view

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
+  resources :restaurants, only: %i[index new create show] do
   # User can see the list of all restaurants
-  get 'restaurants', to: 'restaurants#index'
+  # get 'restaurants', to: 'restaurants#index'
 
   # User can add a new restaurant
-  get 'restaurants/new', to: 'restaurants#new', as: :new_restaurant
-  post 'restaurants', to: 'restaurants#create'
+  # get 'restaurants/new', to: 'restaurants#new', as: :new_restaurant
+  # post 'restaurants', to: 'restaurants#create'
 
   # User can view one restaurant page
-  get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+  # get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+
+    # User can add a new review to a restaurant
+    resources :reviews, only: [:new, :create]
+  end
 end
