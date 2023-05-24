300480e25f168ec5b2e9f4db2c514060d92bb571
Emily Brown <emi.jbrown@gmail.com>
2023-05-24 15:01:32 -0400

    created new restaurant form action/view

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # User can see the list of all restaurants
   get 'restaurants', to: 'restaurants#index'
 
+  # User can add a new restaurant
+  get 'restaurants/new', to: 'restaurants#new', as: :new_restaurant
+  post 'restaurants', to: 'restaurants#create'
 end
